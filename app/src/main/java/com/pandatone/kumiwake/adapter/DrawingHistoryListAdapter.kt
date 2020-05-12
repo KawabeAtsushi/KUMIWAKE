cab39a054bc30355851eea57f64681ba9eb2d95b
Kawabe Atsushi <49773211+KawabeAtsushi@users.noreply.github.com>
2020-05-12 13:43:42 +0900

    add drawing sounds

---
+++ app/src/main/java/com/pandatone/kumiwake/adapter/DrawingHistoryListAdapter.kt
@@ -0,0 +1,66 @@
+package com.pandatone.kumiwake.adapter
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.ListView
+import android.widget.TextView
+import com.pandatone.kumiwake.R
+import java.util.*
+
+/**
+ * Created by atsushi_2 on 2016/04/16.
+ */
+class DrawingHistoryListAdapter(private val context: Context, private val kinds: ArrayList<String>, private val tickets: ArrayList<String>, private val picked: ArrayList<String>) : BaseAdapter() {
+
+    override fun getCount(): Int {
+        return kinds.size
+    }
+
+    override fun getItem(position: Int): String {
+        return kinds[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    @SuppressLint("InflateParams", "SetTextI18n")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val nameTextView: TextView
+        val pickedNoTextView: TextView
+        val remainNoTextView: TextView
+        var v = convertView
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        if (v == null) {
+            v = inflater.inflate(R.layout.row_drawing_history, null)
+        }
+
+        val ticket = kinds[position]
+        nameTextView = v!!.findViewById<View>(R.id.ticketName) as TextView
+        pickedNoTextView = v.findViewById<View>(R.id.pickedNo) as TextView
+        remainNoTextView = v.findViewById<View>(R.id.remainNo) as TextView
+        nameTextView.text = ticket
+        pickedNoTextView.text = pickedNo(ticket).toString()
+        remainNoTextView.text = remainNo(ticket).toString()
+
+        return v
+    }
+
+    private fun pickedNo(ticket:String):Int{
+        return picked.count { it == ticket }
+    }
+
+    private fun remainNo(ticket:String):Int{
+        val pickedNo = picked.count { it == ticket }
+        val allNo = tickets.count { it == ticket }
+        return allNo - pickedNo
+    }
+
+}
+
--- app/src/main/java/com/pandatone/kumiwake/others/drawing/DrawingResult.kt
+++ app/src/main/java/com/pandatone/kumiwake/others/drawing/DrawingResult.kt
@@ -3,20 +3,23 @@ package com.pandatone.kumiwake.others.drawing
 import android.animation.Animator
 import android.animation.AnimatorInflater
 import android.content.Intent
+import android.media.AudioAttributes
+import android.media.SoundPool
 import android.os.Bundle
 import android.os.Handler
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
-import android.widget.ScrollView
-import android.widget.TextView
-import android.widget.Toast
+import android.view.ViewGroup
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.animation.doOnEnd
 import com.airbnb.lottie.LottieAnimationView
 import com.pandatone.kumiwake.MainActivity
 import com.pandatone.kumiwake.PublicMethods
 import com.pandatone.kumiwake.R
+import com.pandatone.kumiwake.adapter.DrawingHistoryListAdapter
+import com.pandatone.kumiwake.member.function.Group
 import com.pandatone.kumiwake.ui.dialogs.DialogWarehouse
 
 
@@ -25,11 +28,17 @@ import com.pandatone.kumiwake.ui.dialogs.DialogWarehouse
  */
 class DrawingResult : AppCompatActivity() {
 
+    private lateinit var soundPool: SoundPool
+    private lateinit var tapHandler: Handler
+    private var soundShake = 0
+    private var soundDraw = 0
     private var prepared = true
     private var v = 0
     private lateinit var shakeAnimator: Animator
     private lateinit var ticketAnimator: Animator
     private lateinit var tickets: ArrayList<String>
+    private var pickedTickets: ArrayList<String> = ArrayList()
+    private lateinit var ticketKinds: ArrayList<String>
     private var pickCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,6 +49,7 @@ class DrawingResult : AppCompatActivity() {
         if (i.getSerializableExtra("tickets") != null) {
             tickets = i.getSerializableExtra("tickets") as ArrayList<String>
         }
+        ticketKinds = ArrayList(tickets.distinct())
         tickets.shuffle()
 
         val ticket = findViewById<TextView>(R.id.ticket)
@@ -48,13 +58,13 @@ class DrawingResult : AppCompatActivity() {
         //progress:0(start)~1(end)
         drawingAnim.setMinProgress(0.5f)
         setUpAnimators(drawingAnim, ticket)
-        shakeAnimation(drawingAnim,pleaseTap)
+        shakeAnimation(drawingAnim, pleaseTap)
 
         drawingAnim.setOnClickListener {
             if (!prepared) {//振動開始
                 ticket.visibility = View.INVISIBLE
                 drawingAnim.progress = 0.5f
-                shakeAnimation(drawingAnim,pleaseTap)
+                shakeAnimation(drawingAnim, pleaseTap)
                 prepared = true
             } else if (!drawingAnim.isAnimating) {//pickup開始
                 pick(ticket)
@@ -68,18 +78,38 @@ class DrawingResult : AppCompatActivity() {
             }
         }
 
-        val layout = findViewById<ConstraintLayout>(R.id.result_view)
-        layout.background = getDrawable(R.drawable.img_others_background)
-
         PublicMethods.showAd(this)
 
-        val scrollView = findViewById<View>(R.id.drawing_scroll) as ScrollView
-        scrollView.post { scrollView.fullScroll(ScrollView.FOCUS_UP) }
-
+        val historyButton = findViewById<Button>(R.id.drawing_history)
+        historyButton.setOnClickListener { onHistory() }
         val showResultButton = findViewById<Button>(R.id.show_result)
         showResultButton.text = getString(R.string.retry)
         showResultButton.setOnClickListener { onRetry() }
         findViewById<Button>(R.id.go_home).setOnClickListener { onGoHome() }
+    }
+
+    //サウンドプールのビルド
+    override fun onResume() {
+        super.onResume()
+        val audioAttributes = AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_UNKNOWN)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build()
+
+        soundPool = SoundPool.Builder()
+                .setAudioAttributes(audioAttributes) // ストリーム数に応じて
+                .setMaxStreams(1)
+                .build()
+
+        // sound.oggをロード
+        soundShake = soundPool.load(this, R.raw.shake, 1)
+        soundDraw = soundPool.load(this, R.raw.draw, 1)
+    }
+
+    //サウンドプールの解放
+    override fun onPause() {
+        super.onPause()
+        soundPool.release()
     }
 
     //アニメーター初期化
@@ -92,25 +122,38 @@ class DrawingResult : AppCompatActivity() {
     }
 
     //待機状態のアニメーション
-    private fun shakeAnimation(drawingAnim: LottieAnimationView,pleaseTap:LottieAnimationView) {
-        //振動
-        shakeAnimator.doOnEnd { shakeAnimator.start() }
+    private fun shakeAnimation(drawingAnim: LottieAnimationView, pleaseTap: LottieAnimationView) {
+        //繰り返し
+        shakeAnimator.doOnEnd {
+            shakeAnimator.start()
+            Handler().postDelayed({
+                // play(ロードしたID, 左音量, 右音量, 優先度, ループ,再生速度)
+                soundPool.play(soundShake, 1.0f, 1.0f, 0, 2, 2.5f)
+            }, 500)
+        }
+        //一回目の処理
         shakeAnimator.start()
+        Handler().postDelayed({
+            soundPool.play(soundShake, 1.0f, 1.0f, 0, 2, 2.5f)
+        }, 500)
+
         //3秒待って進まなかったらタップを促す
-        Handler().postDelayed(Runnable {
-            if(!drawingAnim.isAnimating) {
-                pleaseTap.visibility = View.VISIBLE
-                pleaseTap.playAnimation()
-            }
+        tapHandler = Handler()
+        tapHandler.postDelayed({
+            pleaseTap.visibility = View.VISIBLE
+            pleaseTap.playAnimation()
         }, 3000)
     }
 
     //くじ引き開始
     private fun drawingAnimation() {
         //初期状態に戻す
+        tapHandler.removeCallbacksAndMessages(null)
+        soundPool.stop(soundShake)
         shakeAnimator.start()
         shakeAnimator.pause()
         ticketAnimator.start()
+        soundPool.play(soundDraw, 1.0f, 1.0f, 1, 0, 1.0f)
     }
 
     //アニメーションの途中から結果までスキップ
@@ -120,17 +163,41 @@ class DrawingResult : AppCompatActivity() {
     }
 
     //以下、くじ引き処理
-    private fun pick(ticketTextView: TextView){
-        ticketTextView.text = tickets[pickCount]
-        pickCount ++
-        if (pickCount >= tickets.size){
+    private fun pick(ticketTextView: TextView) {
+        val picked = tickets[pickCount]
+        ticketTextView.text = picked
+        pickedTickets.add(picked)
+        pickCount++
+        if (pickCount >= tickets.size) {
             tickets.shuffle()
             pickCount = 0
         }
-        Toast.makeText(this, "Count : $pickCount",Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Count : $pickCount", Toast.LENGTH_SHORT).show()
     }
 
     //以下、ボタンの処理
+    private fun onHistory() {
+        val builder = AlertDialog.Builder(this)
+        val inflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.drawing_history, findViewById<View>(R.id.info_layout) as ViewGroup?)
+
+        val ticketName = view.findViewById<View>(R.id.infoName) as TextView
+        val historyList = view.findViewById<View>(R.id.historyList) as ListView
+        val okBt = view.findViewById<View>(R.id.okBt) as Button
+
+        builder.setTitle(R.string.history)
+        builder.setView(view)
+
+        val adapter = DrawingHistoryListAdapter(this, ticketKinds, tickets, pickedTickets)
+        val topTxt = pickCount.toString() + " / " + tickets.size
+        ticketName.text = topTxt
+        historyList.adapter = adapter
+
+        val dialog = builder.create()
+        dialog.show()
+        okBt.setOnClickListener { dialog.dismiss() }
+    }
+
     private fun onRetry() {
         v = 0
         val title = getString(R.string.retry_title)
@@ -139,8 +206,6 @@ class DrawingResult : AppCompatActivity() {
     }
 
     private fun retry() {
-        val scrollView = findViewById<View>(R.id.drawing_scroll) as ScrollView
-        scrollView.scrollTo(0, 0)
         Toast.makeText(applicationContext, getText(R.string.retry_finished), Toast.LENGTH_SHORT).show()
     }
 
