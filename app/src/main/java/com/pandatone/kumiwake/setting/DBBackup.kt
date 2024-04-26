2a5d33c16326eba2922f78ae73ac4b09019e9033
TE-AtsushiKawabe <Atsushi.Kawabe@sony.com>
2024-04-26 17:37:12 +0900

    Temp update of back up

--- app/src/main/java/com/pandatone/kumiwake/setting/DBBackup.kt
+++ app/src/main/java/com/pandatone/kumiwake/setting/DBBackup.kt
@@ -1,17 +1,10 @@
 package com.pandatone.kumiwake.setting
 
 import android.annotation.SuppressLint
-import android.content.ContentResolver.MimeTypeInfo
 import android.content.ContentValues.TAG
 import android.content.Context
-import android.content.Intent
 import android.util.Log
-import android.view.View
-import android.webkit.MimeTypeMap
-import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatDialog
-import androidx.core.content.FileProvider
 import com.pandatone.kumiwake.R
 import com.pandatone.kumiwake.adapter.GroupAdapter
 import com.pandatone.kumiwake.adapter.MemberAdapter
@@ -19,15 +12,16 @@ import com.pandatone.kumiwake.history.HistoryAdapter
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileNotFoundException
+import java.io.FileOutputStream
 import java.io.IOException
-import java.util.Locale
 import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
 import java.util.zip.ZipOutputStream
 
 
 @SuppressLint("StaticFieldLeak")
 object DBBackup {
-     private val mb_db_file: String
+    private val mb_db_file: String
         get() {
             MemberAdapter(context).also {
                 it.open()
@@ -55,9 +49,39 @@ object DBBackup {
             return HistoryAdapter.db.path   //DBのディレクトリとファイル名
         }
     private lateinit var context: Context
-    private var b: Boolean = false
 
-    private fun createFolderWithFiles(context: Context, vararg filePaths: String): File? {
+    private fun getFileNameByDB(dbFilePath: String): String {
+        return when (dbFilePath) {
+            File(mb_db_file).name -> "mb.db"
+            File(gp_db_file).name -> "gp.db"
+            File(hs_db_file).name -> "hs.db"
+            else -> {
+                throw Exception("Invalid File")
+            }
+        }
+    }
+
+    private fun getDBByFileName(fileName: String): String {
+        return when (fileName) {
+            "mb.db" -> mb_db_file
+            "gp.db" -> gp_db_file
+            "hs.db" -> hs_db_file
+            else -> {
+                throw Exception("Invalid File Name")
+            }
+        }
+    }
+
+    fun addBackupFiles(c: Context, zipOutputStream: ZipOutputStream) {
+        Log.d(TAG, "addBackupFiles")
+        context = c
+        val backupFile = createFolderWithFiles(context, mb_db_file, gp_db_file, hs_db_file)
+        addFolderToZip(backupFile, zipOutputStream)
+        zipOutputStream.finish()
+        backupFile.copyTo(File(c.getExternalFilesDir(null), "kumiwake_backup"), overwrite = true)
+    }
+
+    private fun createFolderWithFiles(context: Context, vararg filePaths: String): File {
         val folderName = "kumiwake_backup"
         val filesDir = context.getExternalFilesDir(null)
         val folder = File(filesDir, folderName)
@@ -66,14 +90,13 @@ object DBBackup {
             val created = folder.mkdirs()
             if (!created) {
                 Log.e(TAG, "Cannot create backup folder")
-                return null
             }
         }
 
         for (filePath in filePaths) {
             val file = File(filePath)
             if (file.exists()) {
-                val destinationFile = File(folder, file.name)
+                val destinationFile = File(folder, getFileNameByDB(file.name))
                 try {
                     file.copyTo(destinationFile, overwrite = true)
                 } catch (e: IOException) {
@@ -82,26 +105,7 @@ object DBBackup {
             }
         }
 
-        return zipFolder(context, folder)
-    }
-
-    private fun zipFolder(context: Context, folder: File): File? {
-        val zipFileName = "${folder.name}.zip"
-        val zipFile = File(context.cacheDir, zipFileName)
-
-        try {
-            ZipOutputStream(zipFile.outputStream()).use { zipOutputStream ->
-                addFolderToZip(folder, zipOutputStream)
-                zipOutputStream.finish()
-            }
-        } catch (e: IOException) {
-            Log.e(TAG, "Failed to create zip file", e)
-            return null
-        }
-
-        zipFile.copyTo(File(folder.parent, zipFileName), true)
-
-        return zipFile
+        return folder
     }
 
     private fun addFolderToZip(folder: File, zipOutputStream: ZipOutputStream) {
@@ -120,76 +124,23 @@ object DBBackup {
         }
     }
 
-    private fun sendFile(file: File) {
-        val fileUri = FileProvider.getUriForFile(context, "${context.packageName}.provider", file)
-
-        val shareIntent = Intent().apply {
-            action = Intent.ACTION_SEND
-            putExtra(Intent.EXTRA_STREAM, fileUri)
-            type = getMimeType(file)
-            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-        }
-
-        val chooserIntent = Intent.createChooser(shareIntent, "Share via")
-        context.startActivity(chooserIntent)
-    }
-
-    private fun getMimeType(file:File):String{
-            val fileExtension: String =
-                MimeTypeMap.getFileExtensionFromUrl(file.absolutePath)
-            val mimeType = MimeTypeMap.getSingleton()
-                .getMimeTypeFromExtension(fileExtension.lowercase(Locale.getDefault()))
-        return mimeType ?: "*/*"
-        }
-
-    @SuppressLint("SetTextI18n")
-    fun dbBackup(c: Context, dialog: AppCompatDialog) {
+    fun readFile(c: Context, zipInputStream: ZipInputStream) {
         context = c
-        val backupFile = createFolderWithFiles(context, mb_db_file, gp_db_file, hs_db_file)
-        if (backupFile != null) {
-            try {
-                val zipFile = createFolderWithFiles(context, mb_db_file, gp_db_file, hs_db_file)
-                zipFile?.let {
-                    sendFile(zipFile)
-                    Toast.makeText(c, c.getString(R.string.back_up_completed), Toast.LENGTH_SHORT).show()
-                }
-            } catch (e: Exception) {
-                Log.e(TAG, e.toString())
-                Toast.makeText(c, c.getString(R.string.failed_backup), Toast.LENGTH_SHORT).show()
+        val buffer = ByteArray(1024)
+        var entry = zipInputStream.nextEntry
+        while (entry != null) {
+            val fileName = entry.name
+            val file = File(getDBByFileName(fileName))
+            val outputStream = FileOutputStream(file)
+            var len: Int
+            while (zipInputStream.read(buffer).also { len = it } > 0) {
+                outputStream.write(buffer, 0, len)
             }
-        } else {
-            Log.e(TAG, "backup file is null")
-            Toast.makeText(c, c.getString(R.string.failed_backup), Toast.LENGTH_SHORT).show()
+            outputStream.close()
+            entry = zipInputStream.nextEntry
         }
-
-        dialog.dismiss()
-    }
-
-    @SuppressLint("SetTextI18n")
-    fun dbImport(c: Context, path: String, dialog: AppCompatDialog) {
-        context = c
-
-        var err = 0
-        val f = File(path)
-        b = f.exists()           //SDカードにkumiwakeディレクトリがあるか。
-        if (!b) {
-            Toast.makeText(c, c.getString(R.string.not_exist_file), Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        err += fileCopy("$path/mb.db", mb_db_file, c)
-        err += fileCopy("$path/gp.db", gp_db_file, c)//DBのファイルをインポート
-
-        if (err == 0) {
-            dialog.dismiss()
-            Toast.makeText(c, c.getString(R.string.import_completed), Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(c, "Error : $err", Toast.LENGTH_SHORT).show()
-            // メッセージ設定
-            (dialog.findViewById<View>(R.id.dialog_message) as TextView).text =
-                c.getString(R.string.nothing_file) + "\n" + path +
-                        "\n\n" + c.getString(R.string.failed_import)
-        }
+        zipInputStream.closeEntry()
+        zipInputStream.close()
     }
 
 
--- app/src/main/java/com/pandatone/kumiwake/ui/dialogs/FileManagerDialog.kt
+++ app/src/main/java/com/pandatone/kumiwake/ui/dialogs/FileManagerDialog.kt
@@ -1,23 +1,25 @@
 package com.pandatone.kumiwake.ui.dialogs
 
-
-import android.app.Activity
-import android.content.Intent
+import android.content.ContentValues.TAG
+import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.CreateDocument
 import androidx.appcompat.app.AppCompatDialog
 import androidx.fragment.app.DialogFragment
 import com.pandatone.kumiwake.R
 import com.pandatone.kumiwake.setting.DBBackup
-
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
 
 /**
  * Created by atsushi_2 on 2016/11/11.
@@ -29,73 +31,133 @@ class FileManagerDialog(
     private val backup: Boolean
 ) : DialogFragment() {
 
+    private val mimeType = "application/zip"
+
+    private lateinit var writeZipLauncher: ActivityResultLauncher<String>
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        writeZipLauncher =
+            registerForActivityResult(CreateDocument(mimeType)) { uri ->
+                Log.e(TAG, "registerForActivityResult Called")
+                if (uri != null) {
+                    writeZipFile(uri)
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.back_up_completed),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    showWriteErrorDialog()
+                }
+            }
+    }
+
+//    private val readZipLauncher =
+//        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+//            if (uri != null) {
+//                readZipFile(uri)
+//                Toast.makeText(
+//                    requireContext(),
+//                    getString(R.string.import_completed),
+//                    Toast.LENGTH_SHORT
+//                ).show()
+//            } else {
+//                showImportErrorDialog()
+//            }
+//        }
+
     override fun onCreateDialog(savedInstanceState: Bundle?): AppCompatDialog {
-        val dialog = activity?.let { AppCompatDialog(it) }!!
+        val dialog = AppCompatDialog(requireContext())
         // タイトル非表示
-        dialog.window!!.requestFeature(Window.FEATURE_NO_TITLE)
+        dialog.window?.requestFeature(Window.FEATURE_NO_TITLE)
         // フルスクリーン
-        dialog.window!!.setFlags(
+        dialog.window?.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
         )
         dialog.setContentView(R.layout.custom_dialog_layout)
-        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        // タイトル設定
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         (dialog.findViewById<View>(R.id.dialog_title) as TextView).text = mTitle
-        // メッセージ設定
         (dialog.findViewById<View>(R.id.dialog_message) as TextView).text = mMessage
-        // OK ボタンのリスナ
         (dialog.findViewById<View>(R.id.positive_button) as TextView).setOnClickListener {
             if (backup) {
-                activity?.let { it1 -> DBBackup.dbBackup(it1, dialog) }
+                writeZipLauncher.launch("kumiwake_backup.zip")
             } else {
-                setImportErrorView(dialog)
+//                readZipLauncher.launch(mimeType)
             }
+            dismiss()
         }
-        // いいえボタンのリスナ
         (dialog.findViewById<View>(R.id.negative_button) as TextView).setOnClickListener { dismiss() }
 
         return dialog
     }
 
-    //バックアップエラーの際のダイアログ生成
-    private fun setMkdirErrorView(mDialog: AppCompatDialog) {
-        // タイトル設定
-        (mDialog.findViewById<View>(R.id.dialog_title) as TextView).text = getString(R.string.error)
-        // メッセージ設定
-        val pathTextView = mDialog.findViewById<View>(R.id.dialog_path) as TextView
-        pathTextView.visibility = View.VISIBLE
-        (mDialog.findViewById<View>(R.id.dialog_message) as TextView).text =
-            getString(R.string.failed_to_mkdirs)
-        // OKボタンのリスナ
-        (mDialog.findViewById<View>(R.id.positive_button) as TextView).setOnClickListener {
-            activity?.let { it1 -> DBBackup.dbBackup(it1, mDialog) }
-            dismiss()
+    private fun callback(uri: Uri?) {
+        if (uri != null) {
+            writeZipFile(uri)
+            Toast.makeText(
+                requireContext(),
+                getString(R.string.back_up_completed),
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            showWriteErrorDialog()
         }
-        // Cancel ボタンのリスナ
-        (mDialog.findViewById<View>(R.id.negative_button) as TextView).setOnClickListener { dismiss() }
+    }
+
+    private fun writeZipFile(uri: Uri) {
+        requireContext().contentResolver.openOutputStream(uri)?.use { outputStream ->
+            ZipOutputStream(outputStream).use { zipOutputStream ->
+                DBBackup.addBackupFiles(requireContext(), zipOutputStream)
+            }
+        }
+    }
+
+    private fun readZipFile(uri: Uri) {
+        try {
+            val inputStream = requireContext().contentResolver.openInputStream(uri)
+            val zipInputStream = ZipInputStream(inputStream)
+            DBBackup.readFile(requireContext(), zipInputStream)
+            inputStream?.close()
+        } catch (e: Exception) {
+            showImportErrorDialog()
+        }
+    }
+
+    //バックアップエラーの際のダイアログ生成
+    private fun showWriteErrorDialog() {
+        val dialog = AppCompatDialog(requireContext())
+        dialog.setContentView(R.layout.custom_dialog_layout)
+        // タイトル設定
+        (dialog.findViewById<View>(R.id.dialog_title) as TextView).text = getString(R.string.error)
+        // メッセージ設定
+        val pathTextView = dialog.findViewById<View>(R.id.dialog_path) as TextView
+        pathTextView.visibility = View.VISIBLE
+        (dialog.findViewById<View>(R.id.dialog_message) as TextView).text =
+            getString(R.string.failed_to_mkdirs)
+        // OKボタン
+        (dialog.findViewById<View>(R.id.positive_button) as TextView).setOnClickListener { dialog.dismiss() }
+        (dialog.findViewById<View>(R.id.negative_button) as TextView).visibility = View.GONE
+        dialog.show()
     }
 
     //インポートエラーの際のダイアログ生成
-    private fun setImportErrorView(mDialog: AppCompatDialog) {
+    private fun showImportErrorDialog() {
+        val dialog = AppCompatDialog(requireContext())
+        dialog.setContentView(R.layout.custom_dialog_layout)
         // タイトル設定
-        (mDialog.findViewById<View>(R.id.dialog_title) as TextView).text = getString(R.string.error)
+        (dialog.findViewById<View>(R.id.dialog_title) as TextView).text = getString(R.string.error)
         // メッセージ設定
-        val messageTextView = mDialog.findViewById<View>(R.id.dialog_message) as TextView
-        val pathTextView = mDialog.findViewById<View>(R.id.dialog_path) as TextView
+        val messageTextView = dialog.findViewById<View>(R.id.dialog_message) as TextView
+        val pathTextView = dialog.findViewById<View>(R.id.dialog_path) as TextView
         pathTextView.visibility = View.VISIBLE
-        val warningTxt =
-            getString(R.string.nothing_file) +"\n\n" + getString(R.string.failed_import)
-        messageTextView.text = warningTxt
-
-        // OKボタンのリスナ
-        (mDialog.findViewById<View>(R.id.positive_button) as TextView).setOnClickListener {
-            val location = pathTextView.text.toString()
-            activity?.let { it1 -> DBBackup.dbImport(it1, location, mDialog) }
-            dismiss()
+        messageTextView.text = getString(R.string.failed_import)
+        // OKボタン
+        (dialog.findViewById<View>(R.id.positive_button) as TextView).setOnClickListener {
+            dialog.dismiss()
         }
-        // Cancel ボタンのリスナ
-        (mDialog.findViewById<View>(R.id.negative_button) as TextView).setOnClickListener { dismiss() }
+        (dialog.findViewById<View>(R.id.negative_button) as TextView).visibility = View.GONE
+        dialog.show()
     }
-
 }
